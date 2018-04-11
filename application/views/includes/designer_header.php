55b13c487ad9f8b96abef61b0d5c61b550d8253e
prash241 <prashantpandit241@gmail.com>
2018-04-11 13:36:59 +0530

    vendor.php
    small changes

--- application/views/includes/designer_header.php
+++ application/views/includes/designer_header.php
@@ -21,6 +21,7 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/modalreset.css">
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/media-queries.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+       
 	</head>
 	<body>
 
