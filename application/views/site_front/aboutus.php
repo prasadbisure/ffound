a606d6693fee667fff98c0b952a6cf8a71e6f68c
prash241 <prashantpandit241@gmail.com>
2018-05-18 02:25:12 +0530

    detile page chnages

--- application/views/site_front/aboutus.php
+++ application/views/site_front/aboutus.php
@@ -156,7 +156,7 @@ img {
 /*custome*/    
     
 </style>
-<div class="col-lg-12  vendor-search-bar">
+<!--<div class="col-lg-12  vendor-search-bar">
 	<form class="search-box" role="search" method="post" action="/vendorListing">				
 	     <div class="input-group add-on" style=" width: 76%;">
 			<input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
@@ -165,7 +165,7 @@ img {
 				</div>
 		</div>						
 	</form>
-</div>
+</div>-->
 <div class="container">
     <div class="row">
   <div class="leftcolumn">
@@ -182,7 +182,7 @@ img {
   <div class="rightcolumn">
     <div class="card widget">
       <h3><span>About Me</span></h3>
-      <div class="fakeimg" style="height:100px;"><img src="../assets/images/dress/d1.jpg"></div>
+      <div class="fakeimg" style="height:200px;"><img src="../assets/images/dress/d1.jpg"></div>
       <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
     </div>
     <div class="card widget">
@@ -213,68 +213,4 @@ img {
 </div>
 
     
-</div>
-
-<div class="row row-margin-zero itemrow-middle-style">
-    <div class="container datails-style">
-        <div class="col-md-12">
-            <h4 class="item-h-style">Product specilization-</h4>
-        </div>
-        <div class="col-md-12">
-            <h4 class="item2-h-style"><b><?php echo $vendor[0]->specialization; ;?></b></h4>
-            <hr class="itemmiddle-hr">
-        </div>
-        <div class="col-md-12">
-            <p class="item3-h-style">Detailed Bio-</p>
-        </div>
-        <div class="col-md-12">
-            <p class="item-psg">
-              <?php echo $vendor[0]->bio; ;?>
-            </p>
-        </div>
-    </div>
-</div>
-
-<div class="container datails-style product-listing-bottom-margin">
-    <div class="row row-margin-zero">
-        <form action id="review-rating" method="post">
-        <input type="hidden" name="vid" value=<?php echo $vendor[0]->userId; ?> >
-        <div class="col-md-12">
-          <h2><b>Rating &#38; Review</b></h2>
-        </div>
-        <div class="col-md-12">
-            <?php foreach($reviews as $review) { ?>
-                <div class="review-rating col-md-6 col-md-offset-3">
-                    <span class="review-username">
-                        <?php echo $review->name; ?> 
-                        <ul class="star-review">
-                            <?php for($k=1; $k <= $review->rating; $k++ ){ ?>
-                                <li class="secondary-active"><label for="rating_1">&#9733;</label></li>
-                            <?php } ?>
-                            
-                        </ul>
-                    </span>
-                    <p class="review-comment">
-                        <?php echo $review->review; ?>
-                    </p>
-                </div>
-            <?php } ?>
-        </div>
-        <div class="col-md-12">
-            <ul id="star">
-                <li><label for="rating_1">&#9733;</label><input type="radio" name="rating" id="rating_1" class="lable-style" value=1></li>
-                <li><label for="rating_2">&#9733;</label><input type="radio" name="rating" id="rating_2" class="lable-style" value=2></li>
-                <li><label for="rating_3">&#9733;</label><input type="radio" name="rating" id="rating_3" class="lable-style" value=3></li>
-                <li><label for="rating_4">&#9733;</label><input type="radio" name="rating" id="rating_4" class="lable-style" value=4></li>
-                <li><label for="rating_5">&#9733;</label><input type="radio" name="rating" id="rating_5" class="lable-style" value=5></li>
-            </ul>
-        </div>
-        <div class="col-md-12">
-            <textarea rows="4" cols="80" name="review" class="textarea-style"   required></textarea>
-        </div>
-        <div class="col-md-12">
-            <button type="submit" name="SEND" class="btn btn-default submit-btn-style ">Submit</button>
-        </div>
-        </form>
-    </div>
 </div>
