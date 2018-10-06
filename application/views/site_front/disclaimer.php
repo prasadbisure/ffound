5260799e5d9b88434c338fba47ec3fd8dbe64388
prasadbisure <37531281+prasadbisure@users.noreply.github.com>
2018-10-06 09:18:36 +0530

    content changes
    Pivacy, terms, disclaimer

--- application/views/site_front/disclaimer.php
+++ application/views/site_front/disclaimer.php
@@ -4,49 +4,46 @@
 <div class="container">
     <div class="row row-margin-zero about-us-margin">
         <div class="col-md-12 col-padding-zero">
-            <h4 class="about-h-style">ABOUT US</h4>
-            <p class="about-psg-style">
-                Fashion Found is an online world of style and grace for womenswear, founded by Mrunmayee and Shweta.</p>
-            <p class="about-psg-style">As a purveyor of style our intention is to exhibit fashion from all around Pune under one roof. We serve as a platform for upcoming designers and boutiques to unveil their style statements. Our desire is to help connect various fashionista’s and bloggers with all the cool girls in the town.</p>
-            <p class="about-psg-style">We look forward to deliver a hassle free experience to the lovely ladies who can just by sitting home surf and book all the latest style, designs, and new trends that are evolving around various stores and designers.</p>
-            </p>
-        </div>
-    </div>
-    <div class="row row-margin-zero about-us-margin">
-        <div class="col-sm-12 col-padding-zero">
-            <h4 class="about-h-style">WHY US</h4>
-            <h4 class="about-h-style" style="margin-top: 16px;">HAPPY YOU!</h4>
+
+            <h4 class="about-h-style">DISCLAIMER</h4>
+            <p class="about-psg-style">The information contained in this website is for general information purposes only. The information is provided by Fashion Found and while we endeavor to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</p>
+            <p class="about-psg-style">In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.</p>
+            <p class="about-psg-style">Through this website you are able to link to other websites which are not under the control of Fashion Found. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorsement of the views expressed within them.</p>
+            <p class="about-psg-style">Every effort is made to keep the website up and running smoothly. However, Fashion Found takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.</p>
+            <p class="about-psg-style">Kindly make sure that all the information required from you is correctly filled in all requisite forms. We will not be responsible for any problem / discomfort which may arise due to incorrect / false information.</p>
+            <p class="about-psg-style">Also note that colors as shown in the photographs may vary slightly from the actual colors of the products as the process of photography and printing involves a lot of transitions and changes. The colors may also vary from monitor to monitor due to variations in monitor resolution levels. (E.g. The brightness / contrast levels of your monitor may be set too low or too high values).</p>
+
+            <h4>BOOKING</h4>
             <ul class="aboutcontent-ul">
-                <li><span>Meet all the famous designers and boutiques of the town here, at one place.</span></li>
-                <li><span>If you love what you see, just BOOK and get going!</span></li>
-                <li><span>We work with an exquisite notion of FASHION being found easily on the World Wide Web, so all the new fashion, trends and collections for you, without any delay.</span></li>
-            </ul>
-            <h4 class="about-h-style">HAPPY MERCHANT</h4>
-            <ul class="aboutcontent-ul">
-                <li><span>CREATING LOVE by offering an excellent customer experience.</span></li>
-                <li><span>We tend to deliver a homely experience for the user who are trying to reach you.</span></li>
-                <li><span>A perfect bridge between your/ business and the customers.</span></li>
-                <li><span>We offer a huge platform for those who cannot afford building their own website and online marketing.</span></li>
-                <li><span>We get you Happy users who would swoop in and be raptured.</span></li>
-                <li><span>All this with minimal rates.</span></li>
+                <li><span>When a product is booked through our website the booking period granted for all the users is a period of 3 days.</span></li>
+
+                <li><span>And if till then the product is not shopped by the booker from the merchant we are not responsible for the loss.</span></li>
+
+                <li><span>Fashion Found shall not be liable or responsible for any actions or inactions of buyers and sellers or any breach of conditions, representations or warranties by the sellers or manufacturers of the products and expressly disclaims any and all responsibility and liability in that regard. Fashion Found shall not mediate or resolve any dispute or disagreement between the user and the sellers or manufacturers of the products.</span></li>
+
+                <li><span>Fashion Found through the Website only provides a platform that users utilize to communicate and transact with one another and Fashion Found does not endorse any products displayed on the Website or views expressed or information uploaded by any user. In no event Fashion Found shall be liable for any information, content, materials, products or services available through the Website.</span></li>
+
+                <li><span>The right, title, interest and ownership in the products that are transacted by the users through the Website does not vest with Fashion Found and Fashion Found holds no right, title or ownership over such products. The offer and acceptance of products through the Website is at the user’s sole risk and Fashion Found accepts no liability for any errors or omissions or defaults or deficiency of any products either offered or acceptance by the users through the Website.</span></li>
+
+                <li><span>Fashion Found hereby disclaims any guarantees of exactness as to the finish and appearance of the product as ordered by and delivered to the user. The quality or quantity of any products, information or other material purchased or obtained by user through the Website may not meet user’s expectations.</span></li>
+
+                <li><span>The Website could also be linked to third party websites and Fashion Found assumes no responsibility for results obtained from the use of these websites.</span></li>
+
+                <li><span>Fashion Found makes no assurances that the Website is bug free and user friendly or the safety of the use of the Website. Fashion Found including affiliates, associates and their respective owners, directors, officers, agents, shareholders and partners shall not be liable in any manner with respect to the loss or damage  incurred or suffered by the users on account of virus attacks or bugs or other software or programs that may be present or embedded while using or accessing the Website.</span></li>
+
+                <li><span>Fashion Found does not guarantee that the services provided through the Website will function without interruption or errors. In particular the operation of the services may be interrupted due to maintenance, updates, or system failures, virus attacks, bugs or due to any other technical reasons or otherwise. Website disclaims all liability for losses or damages caused by any such interruption or errors in functioning. Fashion Found also disclaims all liability for any malfunctioning impossibility of access or poor use conditions of the Website due to inappropriate equipment, disturbances related to internet services, down time or otherwise.</span></li>
+
+                <li><span>To the maximum extent permitted by applicable law, Fashion Found will have no liability related to user content arising under intellectual property rights, libel, privacy, publicity, obscenity or other laws. Fashion Found also disclaims all liability with respect to the misuse, loss, modification or unavailability of any user content.</span></li>
+
+                <li><span>When the user deletes any kind of information the user must understand that the removed content may continue in backup copies.</span></li>
             </ul>
 
-        </div>
-    </div>
-    <div class="row row-margin-zero about-us-margin">
-        <div class="col-sm-12 col-padding-zero">
-            <h4 class="about-h-style">WHAT WE DO</h4>
-            <p class="about-psg-style">
-                Fashion Found’s tailor made website works with the moto to advertise and display fashion online, so as
-                to be easily found.</p>
-            <p class="about-psg-style">
-                Particularly we have sections for Boutiques and Designers and also a Directory to exhibit the products
-                that would woo the visitor.
-                We not only display fashion on our portal but also advertise and optimize them through social media
-                and SEO.</p>
-            <p class="about-psg-style">With us you can take your business to the next level, all you need is an up to date profile of your
-                business on our portal and we take care of the rest.
-            </p>
+            <h4 class="about-h-style">CANCELLATION</h4>
+            <p class="about-psg-style">Cancellations can be made within 24 hours of booking an order. The order may be cancelled by addressing an email to the respective Designer/ Boutique or by calling and cancelling the order at the given respective contact number.</p>
+
+            <h4 class="about-h-style">SIZING</h4>
+            <p class="about-psg-style">The fit of all the clothing would depend on its cut, style, pattern and fabric of each garment.</p>
+
         </div>
     </div>
 </div>
--- application/views/site_front/privacyAndPolicy.php
+++ application/views/site_front/privacyAndPolicy.php
@@ -4,49 +4,86 @@
 <div class="container">
     <div class="row row-margin-zero about-us-margin">
         <div class="col-md-12 col-padding-zero">
-            <h4 class="about-h-style">ABOUT US</h4>
+            <h4 class="about-h-style">PIVACY AND POLICY</h4>
             <p class="about-psg-style">
-                Fashion Found is an online world of style and grace for womenswear, founded by Mrunmayee and Shweta.</p>
-            <p class="about-psg-style">As a purveyor of style our intention is to exhibit fashion from all around Pune under one roof. We serve as a platform for upcoming designers and boutiques to unveil their style statements. Our desire is to help connect various fashionista’s and bloggers with all the cool girls in the town.</p>
-            <p class="about-psg-style">We look forward to deliver a hassle free experience to the lovely ladies who can just by sitting home surf and book all the latest style, designs, and new trends that are evolving around various stores and designers.</p>
-            </p>
+                This Privacy Policy governs the manner in which Fashion Found collects, uses, maintains and discloses information collected from users (each, a "User") of the www.fashionfound.in website ("Site"). This privacy policy applies to the Site and all products and services offered by Fashion Found.  </p>
         </div>
     </div>
     <div class="row row-margin-zero about-us-margin">
         <div class="col-sm-12 col-padding-zero">
-            <h4 class="about-h-style">WHY US</h4>
-            <h4 class="about-h-style" style="margin-top: 16px;">HAPPY YOU!</h4>
+            <h4 class="about-h-style">Personal Identification Information</h4>
             <ul class="aboutcontent-ul">
-                <li><span>Meet all the famous designers and boutiques of the town here, at one place.</span></li>
-                <li><span>If you love what you see, just BOOK and get going!</span></li>
-                <li><span>We work with an exquisite notion of FASHION being found easily on the World Wide Web, so all the new fashion, trends and collections for you, without any delay.</span></li>
+                <li>
+                    <span>We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, register on the site and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address, mailing address, phone number, credit/debit card information, Users may, however, visit our Site anonymously.</span>
+                </li>
+                <li>
+                    <span>We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</span>
+                </li>
+
             </ul>
-            <h4 class="about-h-style">HAPPY MERCHANT</h4>
+            <h4 class="about-h-style">Non-personal Identification Information</h4>
+            <p class="about-psg-style">
+                We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information, such as the operating system and the Internet service providers’ utilized and other similar information.
+            </p>
+
+            <h4 class="about-h-style">How we use collected information</h4>
             <ul class="aboutcontent-ul">
-                <li><span>CREATING LOVE by offering an excellent customer experience.</span></li>
-                <li><span>We tend to deliver a homely experience for the user who are trying to reach you.</span></li>
-                <li><span>A perfect bridge between your/ business and the customers.</span></li>
-                <li><span>We offer a huge platform for those who cannot afford building their own website and online marketing.</span></li>
-                <li><span>We get you Happy users who would swoop in and be raptured.</span></li>
-                <li><span>All this with minimal rates.</span></li>
+                <li>
+                    <span>Fashion Found collects and uses Users personal information for the following purposes:</span>
+                </li>
+                <li>
+                    <span>To improve customer service</span>
+                </li>
+                <li>
+                    <span>Your information helps us to more effectively respond to your customer service requests and support needs.</span>
+                </li>
+                <li>
+                    <span>To personalize user experience</span>
+                </li>
+                <li>
+                    <span>We may use information in the aggregate to understand how our Users as a group use the services and resources provided on our Site.</span>
+                </li>
             </ul>
+
+            <h4 class="about-h-style">To improve our Site</h4>
+            <p class="about-psg-style">We continually strive to improve our website offerings based on the information and feedback we receive from you.</p>
+
+            <h4 class="about-h-style">To process transactions</h4>
+            <ul class="aboutcontent-ul">
+                <li>
+                    <span>We may use the information Users provide about themselves when placing an order only to provide service to that order. We do not share this information with outside parties except to the extent necessary to provide the service.</span>
+                </li>
+                <li>
+                    <span>To send Users information they agreed to receive about topics we think will be of interest to them.</span>
+                </li>
+                <li>
+                    <span>To send periodic emails</span>
+                </li>
+                <li>
+                    <span>The email address Users provide for order processing, will only be used to send them information and updates pertaining to their order. It may also be used to respond to their inquiries, and/or other requests or questions. If User decides to opt-in to our mailing list, they will receive emails that may include company updates, related product or service information, etc. If at any time the User would like to unsubscribe from receiving future emails, we include and unsubscribe link at the bottom of each email.</span>
+                </li>
+            </ul>
+
+            <h4 class="about-h-style">How we protect your information</h4>
+            <ul class="aboutcontent-ul">
+                <li><span>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</span></li>
+                <li><span>Sensitive and private data exchange between the Site and its Users happens over a secured communication channel and is encrypted and protected with digital signatures.</span></li>
+            </ul>
+
+            <h4 class="about-h-style">Sharing your personal information</h4>
+            <p class="about-psg-style">We do not sell, trade, or rent Users personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates and advertisers for the purposes outlined above. We may use third party service providers to help us operate our business and the Site or administer activities on our behalf, such as sending out newsletters. We may share your information with these third parties for those limited purposes provided that you have given us your permission.</p>
+
+            <h4 class="about-h-style">Changes to this privacy policy</h4>
+            <p class="about-psg-style">Fashion Found has the discretion to update this privacy policy at any time. When we do, revise the updated date at the bottom of this page, We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+
+            <h4 class="about-h-style">Your acceptance of these terms</h4>
+            <p class="about-psg-style">By using this Site, you signify your acceptance of this policy and if you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.</p>
+
+            <h4 class="about-h-style">Contacting us</h4>
+            <p class="about-psg-style">If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us at: info@fashionfound.in</p>
+
 
         </div>
     </div>
-    <div class="row row-margin-zero about-us-margin">
-        <div class="col-sm-12 col-padding-zero">
-            <h4 class="about-h-style">WHAT WE DO</h4>
-            <p class="about-psg-style">
-                Fashion Found’s tailor made website works with the moto to advertise and display fashion online, so as
-                to be easily found.</p>
-            <p class="about-psg-style">
-                Particularly we have sections for Boutiques and Designers and also a Directory to exhibit the products
-                that would woo the visitor.
-                We not only display fashion on our portal but also advertise and optimize them through social media
-                and SEO.</p>
-            <p class="about-psg-style">With us you can take your business to the next level, all you need is an up to date profile of your
-                business on our portal and we take care of the rest.
-            </p>
-        </div>
-    </div>
+
 </div>
